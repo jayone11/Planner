6aeec0e0ac1fbed7b8c84a682a0105eb03c51038
JBTechDev <jbtechdev@gmail.com>
2019-10-18 16:08:41 -0700

    dynamically generated section rows using jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,52 @@
+$(document).ready(function() {
 const planner = document.getElementById("planner");
-const date = document.getElementById("date");
 
-const newDate = moment().format('MMMM Do YYYY')
-date.textContent += newDate;
-console.log(newDate);
+// Display current day near top of container
+function displayTodaysDate() {
+    const date = document.getElementById("date");
+    const newDate = moment().format('MMMM Do YYYY')
+    date.textContent += newDate;
+    console.log(newDate);    
+}
+displayTodaysDate();
 
+// Create dynamic elements
+function generateRow() {
+    let hoursInDay = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+    for (i = 0; i < hoursInDay.length; i++) {
+        console.log(hoursInDay[i]);
+
+        let plannerDiv = $('<div>')
+        plannerDiv.addClass('planner')
+        $(".section").append($(plannerDiv))
+
+        let timeDiv = $('<div>')
+        timeDiv.text(hoursInDay[i])
+        timeDiv.addClass('plannerItems time')
+        $(plannerDiv).append(timeDiv)
+
+        let input = $('<input>')
+        input.addClass('plannerItems activity')
+        input.attr('placeholder', 'Enter activity here')
+        $(timeDiv).after(input)
+        console.log(timeDiv)
+
+        let saveBtnDiv = $('<div>')
+        saveBtnDiv.addClass('plannerItems action')
+        $(input).after(saveBtnDiv)
+
+        let saveBtn = $('<button>')
+        saveBtn.addClass('save-button')
+        saveBtn.text("Save")
+        $(saveBtnDiv).append(saveBtn)
+    }
+}
+generateRow();
 // Save events for each hour of the day.
+// var activityValue = $("#activity").val();
+// console.log("Log1", activityValue);
 
+});
 // Dynamically updated HTML and CSS powered by jQuery.
 
 // Display standard business hours 9am to 5pm.
@@ -16,7 +56,7 @@ console.log(newDate);
 
 // Clicking on the button will store the time and user input in localStorage.
 
-// Display current day near top of container
+
 
 // Each hour should be color coded to reflect if time slot is in the past, the present, or the future.
 // This will change depending on the time of day.
