089c8aff76c9e2354fd34d884f9cb4d2dd486602
JBTechDev <jbtechdev@gmail.com>
2020-01-02 12:01:44 -0800

    dev branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,155 @@
 $(document).ready(function() {
-const planner = document.getElementById("planner");
+    const planner = document.getElementById("planner");
 
-// Display current day near top of container
-function displayTodaysDate() {
-    const date = document.getElementById("date");
-    const newDate = moment().format('MMMM Do YYYY')
-    date.textContent += newDate;
-    console.log(newDate);    
-}
-displayTodaysDate();
+    // Display current day near top of container
+    function displayTodaysDate() {
+        const newDate = moment().format('MMMM Do YYYY')
+        $(".date").html(newDate);
+        console.log(newDate);    
+    }
+    displayTodaysDate();
 
-// Create dynamic elements
-// Display standard business hours 9am to 5pm.
-function generateRow() {
+    // Create dynamic elements
+    // Display standard business hours 9am to 5pm.
     // Each time slot is one hour.
-    let hoursInDay = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+    const hoursInDay = [];
+    // let hoursInDay = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM","5:00 PM"];
     // Each slot contains the time, user input field, and save button.
     // Dynamically updated HTML and CSS powered by jQuery.
-    for (i = 0; i < hoursInDay.length; i++) {
-        console.log(hoursInDay[i]);
-        
-        // Create Planner div.
-        let plannerDiv = $('<div>')
-        plannerDiv.addClass('planner')
-        $(".section").append($(plannerDiv))
-       
-        // Create Time div.
-        let timeDiv = $('<div>')
-        
-        // Passing in each hour from the array
-        timeDiv.text(hoursInDay[i])
-        timeDiv.addClass('plannerItems time')
-        $(plannerDiv).append(timeDiv)
-        
-        // Create Input element.
-        let input = $('<input>')
-        input.addClass('plannerItems activity')
-        input.attr('placeholder', 'Enter activity here')
-        $(timeDiv).after(input)
-        
-        // Create Save button div.
-        let saveBtnDiv = $('<div>')
-        saveBtnDiv.addClass('plannerItems action')
-        $(input).after(saveBtnDiv)
-        
-        // Create Save button element
-        let saveBtn = $('<button>')
-        saveBtn.addClass('save-button')
-        saveBtn.text('Save')
-        $(saveBtnDiv).append(saveBtn)
-        var outputDiv = "";
-        $(saveBtn).click(function() {
-            outputDiv = $('<p>')
-            outputDiv.text(input.val())
-            outputDiv.addClass("output")
-            let test = $(input).val()
-            localStorage.setItem("Activity", test)
-            $(input).append(outputDiv)
-            $(input).prop("readonly", true)
-            // $(this).attr("disabled", true)
-            $(this).css("background-color", "red")
-            $(this).text("Delete")
-            // Delete button removes prior activity and restores input to editable
-            $(this).click(function() {
-                $(input).text(input.val(""))
-                $(input).prop("readonly", false)
-                $(this).text('Save')
-                $(this).css("background-color", "rgb(77, 154, 248)")
-            })
-        })
+    // for (i = 0; i < hoursInDay.length; i++) {
+    for (var hour = 0; hour < 24; hour++) {
+        hoursInDay.push(moment({
+            hour
+        }).format('h:mm  a'));
+        $('.section').append(`<div id="planner" class="planner" data-time=${hour}>
+                <div class="plannerItems time" data-time=${hour}>
+                    <p>${moment({hour}).format('h:mm  a')}</p>
+                </div>  
+                <div class= "description" data-time=${hour}>  
+                <textarea class="plannerItems activity" placeholder="Enter activity here"></textarea>
+                </div>
+                <div class="plannerItems action" id="action" data-time=${hour}>
+                    <button class="save-button" id="save-button" data-time=${hour}>Save</button>
+                </div>
+            </div>`);
+        // // Create Planner div.
+        // let plannerDiv = $('<div>');
+        // plannerDiv.addClass('planner');
+        // plannerDiv.attr("data-time", hour);
+        // $(".section").append($(plannerDiv));
+ 
+        // // Create Time div.
+        // let timeDiv = $('<div>');
+        // // Passing in each hour from the array
+        // timeDiv.addClass('plannerItems time');
+        // timeDiv.attr("data-time", hour);
+        // $(plannerDiv).append(timeDiv);
 
-    function renderLastActivity() {
-        let activity = localStorage.getItem("Activity");
-        if (activity === null) {
-            return;
-        }
-        $('.output').text(activity);
-        }
-    renderLastActivity();
+        // // Create paragraph element.
+        // let pElement = $('<p>');
+        // let text = moment({hour}).format('h:mm a');
+        // pElement.text(text);
+        // $(timeDiv).append(pElement);
+
+        // // Create Input element.
+        // let input = $('<textarea>');
+        // input.addClass('plannerItems activity textArea');
+        // input.attr('placeholder', 'Enter activity here');
+        // input.attr('data-time', hour);
+        // $(timeDiv).after(input);
+        
+        // // Create Save button div.
+        // let saveBtnDiv = $('<div>');
+        // saveBtnDiv.addClass('plannerItems action');
+        // saveBtnDiv.attr('data-time', hour);
+        // $(input).after(saveBtnDiv);
+        
+        // // Create Save button element
+        // let saveBtn = $('<button>');
+        // saveBtn.addClass('save-button');
+        // saveBtn.text('Save');
+        // $(saveBtnDiv).append(saveBtn);
+        // var outputDiv;
+        // $(saveBtn).click(function() {
+        //     outputDiv = $('<p>');
+        //     outputDiv.text(input.val());
+        //     outputDiv.addClass("output");
+        //     // let test = $(input).val()
+        //     // localStorage.setItem("Activity", test)
+        //     $(input).append(outputDiv);
+        //     $(input).prop("readonly", true);
+        //     // $(this).attr("disabled", true)
+        //     // $(this).css("background-color", "red")
+        //     // $(this).text("Delete")
+            
+        //     // Delete button removes prior activity and restores input to editable
+        //     // $(this).click(function() {
+        //     //     $(input).text(input.val(""))
+        //     //     $(input).prop("readonly", false)
+        //     //     $(this).text('Save')
+        //     //     $(this).css("background-color", "rgb(77, 154, 248)")
+        //     // })
+        //     // renderLastActivity();
+        // });
+
+        // // function renderLastActivity() {
+        // //     let activity = localStorage.getItem("Activity");
+        // //     if (activity === null) {
+        // //         return;
+        // //     }
+        // //     $('.output').text(activity);
+        // // }
     }
-}
-generateRow();
+
+    var m = moment();
+    $.each($(".planner"), function (index, value) {
+        let dateHour = $(this).attr("data-time");
+        if (Number(dateHour) === m.hour()) {
+            $(this).find("textarea").addClass('present');
+        } else if (Number(dateHour) < m.hour()) {
+            $(this).find("textarea").addClass('past');
+        } else {
+            $(this).find("textarea").addClass('future');
+        }
+    });
+
+    //Check for local storage to set value to the object
+    let timeObject = {};
+        if (localStorage.getItem('timeObject')) {
+        timeObject = JSON.parse(localStorage.getItem('timeObject'));
+    } else {
+        timeObject = {
+            '9': { time: "9", value: ""},
+            '10':{ time: "10", value: ""},
+            '11':{ time: "11", value: ""},
+            '12':{ time: "12", value: ""},
+            '13':{ time: "13", value: ""},
+            '14':{ time: "14", value: ""},
+            '15':{ time: "15", value: ""},
+            '16':{ time: "16", value: ""},
+            '17':{ time: "17", value: ""}
+        };
+    }
+    $("textarea").each(function(){
+        console.log(this)
+        $(this).val(timeObject[$(this).attr("data-time")].value);
+        console.log(this)
+   });
+
+    $(document).on('click', '.save-button', function(event){
+        var data = $(this).attr("data-time") 
+        console.log('alert' + data);
+        var timeValue = $(this).attr("data-time");
+        var textValue = $(".planner").find(".textArea").val();
+        timeObject[timeValue].value = textValue;
+   
+      localStorage.setItem('timeObject', JSON.stringify(timeObject));
+  });
 
 });
+
+
+
 // Save events for each hour of the day.
 // $('.activity').val().append()
 // console.log(activityValue)
